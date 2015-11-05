a5dc395c22013e29999f29943576a738fd984b48
István Antal <istvan.m.antal@gmail.com>
2015-11-05 16:12:42 +0200

    Remember hide resolved checked state

---
+++ web/js/main.js
@@ -0,0 +1,20 @@
+$(function () {
+    var localStorage = window.localStorage || {};
+    
+    $('.ui.checkbox').checkbox();
+    $('.ui.progress').progress();
+
+    $('.ux-issue-resolved-toggle').change(function () {
+        localStorage.hideResolvedIssues = $(this).is(':checked');
+        
+        if (localStorage.hideResolvedIssues) {
+            $('.ux-issue-resolved').addClass('hidden');
+        } else {
+            $('.ux-issue-resolved').removeClass('hidden');
+        }
+    });
+    
+    if (localStorage.hideResolvedIssues) {
+        $('.ux-issue-resolved-toggle').attr('checked', true).trigger('change');
+    }
+});
