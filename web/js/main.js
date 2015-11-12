92d4edde07c218497ec7b0e9f7c04abd886f5d0e
István Antal <istvan.m.antal@gmail.com>
2015-11-12 11:52:19 +0000

    String compare in hide resolved issues

--- web/js/main.js
+++ web/js/main.js
@@ -7,14 +7,14 @@ $(function () {
     $('.ux-issue-resolved-toggle').change(function () {
         localStorage.hideResolvedIssues = $(this).is(':checked');
         
-        if (localStorage.hideResolvedIssues) {
+        if (localStorage.hideResolvedIssues === 'true') {
             $('.ux-issue-resolved').addClass('hidden');
         } else {
             $('.ux-issue-resolved').removeClass('hidden');
         }
     });
     
-    if (localStorage.hideResolvedIssues) {
+    if (localStorage.hideResolvedIssues === 'true') {
         $('.ux-issue-resolved-toggle').attr('checked', true).trigger('change');
     }
 });
