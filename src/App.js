aa730c02c23bf88c55f29b0a2f8052fff49e05c2
István Antal <istvan.m.antal@gmail.com>
2018-01-31 05:44:47 +0000

    Initial prototype

--- src/App.js
+++ src/App.js
@@ -1,32 +1,88 @@
 import React, { Component } from 'react';
 import './App.css';
 
+const sleep = async (timeout) => new Promise(resolve => {
+  setTimeout(timeout, resolve);
+})
+
+const fetchJson = async (url, retryCount = 5) => {
+  try {
+    return fetch(url).then(response => response.json());
+  } catch (e) {
+    if (!retryCount) {
+      throw e;
+    }
+    await sleep(1000);
+    return fetchJson(url, retryCount - 1);
+  }
+}
+
+const IssueRow = ({ widget, issue, baseUrl }) => {
+  let classes = '';
+
+  if (widget.doneStates.includes(issue.fields.status.name)) {
+    classes += 'done ';
+  }
+
+  if (widget.blockedStates.includes(issue.fields.status.name)) {
+    classes += 'blocked ';
+  }
+
+  return (
+    <tr className={classes}>
+      <td>
+        <a href={`${baseUrl}/browse/${issue.key}`} target="_blank">
+          {issue.fields.summary}
+        </a>
+      </td>
+      <td>
+        {issue.fields.assignee && <strong>
+          <img title={issue.fields.assignee.displayName} width="24" height="24" src={issue.fields.assignee.avatarUrls['24x24']} />
+        </strong>}
+      </td>
+    </tr>
+  );
+};
+
 class App extends Component {
   constructor() {
     super();
 
     this.state = {
-      issuesInProgress: {
-        issues: []
-      },
+      widgets: [],
     };
 
-    fetch('data/in-progress.json')
-      .then(response => response.json())
-      .then((data) => {
-        this.setState({
-          issuesInProgress: data,
-        });
+    (async () => {
+      const data = await fetch('settings.json').then(response => response.json())
+
+      const widgets = await Promise.all(data.widgets.map(async (widget, key) => ({
+        ...widget,
+        data: await fetchJson(`data/${key}.json`)
+      })))
+
+      this.setState({
+        baseUrl: data.url,
+        widgets,
       });
+    })();
   }
   render() {
     return (
       <div className="App">
-        <h1>In progress</h1>
-        {this.state.issuesInProgress.issues.map(issue => (
-          <div key={issue.key}>
-            {issue.fields.summary}
-            {issue.fields.assignee && <strong>{issue.fields.assignee.displayName}</strong>}
+        {this.state.widgets.map((widget, key) => (
+          <div className="widget" key={key}>
+            <table>
+              <thead>
+                <tr>
+                  <th colSpan="2">{widget.title}</th>
+                </tr>
+              </thead>
+              <tbody>
+                {widget.data.issues.sort((a, b) => widget.doneStates.includes(a.fields.status.name) ? 1 : -1).map(issue => (
+                  <IssueRow widget={widget} issue={issue} baseUrl={this.state.baseUrl} key={issue.key} />
+              ))}
+              </tbody>
+            </table>
           </div>
         ))}
       </div>
