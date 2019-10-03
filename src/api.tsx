2284317e17fd494da1160d17f972c61e6a9f1ac5
István Antal <i@istvan.xyz>
2019-10-03 07:58:21 +0100

    Parse gql

--- src/api.tsx
+++ src/api.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import GraphiQL from 'graphiql';
 import { Observable } from 'apollo-link';
+import { parse } from 'graphql/language/parser';
 
 import './api.scss';
 
-const runQuery = (params: {}) => new Observable(subscriber => {
-    console.log(params);
+const runQuery = (params: { query: string }) => new Observable(subscriber => {
+    console.log(parse(params.query));
     subscriber.next({ data: {} });
 });
 
